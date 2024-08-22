83a1b42071b8b762db226b74a7f739f0774ee773
mores_rafael <moraes_rafael@terceiros.sicredi.com.br>
2024-08-22 16:38:55 -0300

    add: change userEntity and repository

--- src/main/java/estudos/spring_secutiry_auth/repository/UserRepository.java
+++ src/main/java/estudos/spring_secutiry_auth/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package estudos.spring_secutiry_auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import estudos.spring_secutiry_auth.repository.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, String> {
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
 }
--- src/main/java/estudos/spring_secutiry_auth/repository/entity/UserEntity.java
+++ src/main/java/estudos/spring_secutiry_auth/repository/entity/UserEntity.java
@@ -1,25 +1,73 @@
 package estudos.spring_secutiry_auth.repository.entity;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Table(name = "users")
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
-    private String username;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "username"))
-    @Column(name = "role")
-    private Set<String> roles = new HashSet<>();
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
